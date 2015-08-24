c0c4557456459a8254edd8a47fcec19eb82d2adc
St33v <github@f3rr3t.com>
2015-08-24 12:35:53 +1000

    try again

--- kmlstitch.sh
+++ kmlstitch.sh
@@ -4,3 +4,22 @@
 # name the data directory
 datadir='../data/'
 echo $datadir
+
+# Functions ##########################
+
+readKml()
+{
+	echo $file
+}
+
+######################################
+
+
+for file in '$datadir'*
+do
+	echo  'file is $file\n'
+done
+
+echo "finished"	
+
+ls -f $datadir | while read -r file; do wc "$file"; done 
